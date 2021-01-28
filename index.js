cf204ccf8ce21a2f43b1f68135c4ebca946d61e0
Thomas Domingo <tdomingo90@gmail.com>
2021-01-27 22:19:26 -0800

    :q

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/api/galleries', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/api/listings', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: 'http://localhost:3003', changeOrigin: true }));
+app.use('/api/more', createProxyMiddleware({ target: 'http://localhost:3004', changeOrigin: true }));
+
+
+app.listen(port, () => { console.log(`Proxy running on port ${port}`)});
